7c3f855eed02397edc54e4f917b45449a39fad95
Erik <erikwjonsson@gmail.com>
2018-02-23 15:19:34 +0100

    Removed some comments from migration.

--- db/migrate/20180218173708_create_debtors.rb
+++ db/migrate/20180218173708_create_debtors.rb
@@ -14,15 +14,15 @@ class CreateDebtors < ActiveRecord::Migration[5.1]
       t.string :zip
       t.string :city
       t.string :street
-      t.string :house_number # maybe can be string? eg 2A
+      t.string :house_number
       #t.string :second_adress_line
-      t.string :phone_number # not sure if shoudl be string or itneger, check best pracice
-      #t.string :mobile_phone_number # not sure if shoudl be string or itneger, check best pracice
+      t.string :phone_number
+      #t.string :mobile_phone_number
       t.string :email_address
       t.string :sap_invoice_number
       t.string :fixed_value
       t.float :amount # (with ISO currency code)
-      t.timestamp :date_of_export_to_debt_collection# Date of export to debt collection
+      t.timestamp :date_of_export_to_debt_collection
 
       t.timestamps
     end
