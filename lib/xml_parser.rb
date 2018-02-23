6125dcb83795495ad4022ea1272fb5af55895323
Erik <erikwjonsson@gmail.com>
2018-02-23 14:35:10 +0100

    Cleaned up xml_parser from comments and unnecessary require calls. - Corrected mispelling. Address, not adress.

--- lib/xml_parser.rb
+++ lib/xml_parser.rb
@@ -1,8 +1,3 @@
-require 'nokogiri' # remove this later
-require 'pry'
-require 'xpath'
-#File.load('./test-data.xml')[i].content
-
 module XmlParser
 	def self.parse(file)
 		doc = Nokogiri::XML(File.open(file)) # File.open("./test-data.xml")
@@ -12,53 +7,35 @@ module XmlParser
 	private
 
 	def self.hashify(doc)
-		#hash = Nori.new(:parser => :rexml).parse(File.read('./test-data.xml'))
-		#debtors = hash['n0:CommissionedOutstandingCollectionsERPBulkRequest']['CommissionedOutstandingCollectionsERPRequestMessage']
-
-
-
 		debtors = doc.xpath('//CommissionedOutstandingCollectionsERPRequestMessage')
+		address = '//CommissionedOutstandingCollections//DebtorParty//Address'
+		physical_address = '//CommissionedOutstandingCollections//DebtorParty//Address'
 		debtors.each_with_index.map do |collection, i|
-
-
-		              #<NumberDefaultIndicator>true</NumberDefaultIndicator>
-		              #<NumberDescription languageCode="en"/>
-		              #<NumberUsageDenialIndicator>false</NumberUsageDenialIndicator>
-
-		              #<URIDefaultIndicator>true</URIDefaultIndicator>
-		              #<URIDescription languageCode="en"/>
-		              #<URIUsageDenialIndicator>false</URIUsageDenialIndicator>
-		              # design note: 
-		            	# after din
-			adress = '//CommissionedOutstandingCollections//DebtorParty//Address'
-			physical_adress = '//CommissionedOutstandingCollections//DebtorParty//Address'
 			{
-
 				system_id: collection.xpath('//CommissionedOutstandingCollections//ID')[i].content,
 				customer_number: collection.xpath('//CommissionedOutstandingCollections//DebtorParty//DebtorID')[i].content,
-
-				gender: collection.xpath("#{adress}//PersonName//FormOfAddressCode")[i].content,
-				first_name: collection.xpath("#{adress}//PersonName//GivenName")[i].content,
-				last_name: collection.xpath("#{adress}//PersonName//FamilyName")[i].content,
-				#academic_title: collection.xpath("#{physical_adress}//AcademicTitleCode")[i].content,
-				#academic_title_2: collection.xpath("#{physical_adress}//AcademicTitleCode")[i].content,
+				gender: collection.xpath("#{address}//PersonName//FormOfAddressCode")[i].content,
+				first_name: collection.xpath("#{address}//PersonName//GivenName")[i].content,
+				last_name: collection.xpath("#{address}//PersonName//FamilyName")[i].content,
+				#academic_title: collection.xpath("#{physical_address}//AcademicTitleCode")[i].content,
+				#academic_title_2: collection.xpath("#{physical_address}//AcademicTitleCode")[i].content,
 				# rename academic_title_2
 				# ACADEMIC TITES DOES NOT SEEM TO EXIST IN 
-				iso_code_language: collection.xpath("#{physical_adress}//CountryName")[i].content,
+				iso_code_language: collection.xpath("#{physical_address}//CountryName")[i].content,
 				# above: why did i not need to require this    CountryName languageCode="de"
-				iso_code_communication_language: collection.xpath("#{adress}//Communication//CorrespondenceLanguageName")[i].attributes['languageCode'].value,
-				iso_code_address_country: collection.xpath("#{physical_adress}//CountryCode")[i].content,
-				zip: collection.xpath("#{physical_adress}//StreetPostalCode")[i].content,
-				city: collection.xpath("#{physical_adress}//CityName")[i].content,
-				street: collection.xpath("#{physical_adress}//StreetName")[i].content,
-				house_number: collection.xpath("#{physical_adress}//StreetName")[i].content,
-				#second_adress_line: collection.xpath("#{physical_adress}//CareOfName")[i].content,
+				iso_code_communication_language: collection.xpath("#{address}//Communication//CorrespondenceLanguageName")[i].attributes['languageCode'].value,
+				iso_code_address_country: collection.xpath("#{physical_address}//CountryCode")[i].content,
+				zip: collection.xpath("#{physical_address}//StreetPostalCode")[i].content,
+				city: collection.xpath("#{physical_address}//CityName")[i].content,
+				street: collection.xpath("#{physical_address}//StreetName")[i].content,
+				house_number: collection.xpath("#{physical_address}//StreetName")[i].content,
+				#second_address_line: collection.xpath("#{physical_address}//CareOfName")[i].content,
 				# CareOfName does not seem to exist in test-data.xml
-				phone_number: collection.xpath("#{adress}//Telephone//SubscriberID")[i].content,
+				phone_number: collection.xpath("#{address}//Telephone//SubscriberID")[i].content,
 				# review above
-				#mobile_phone_number: collection.xpath("#{adress}//Communication//MobilePhone")[i].content,
+				#mobile_phone_number: collection.xpath("#{address}//Communication//MobilePhone")[i].content,
 				# MobilePhone does not seem to exist.
-				email_address: collection.xpath("#{adress}//Communication//Email//URI")[i].content,
+				email_address: collection.xpath("#{address}//Communication//Email//URI")[i].content,
 				sap_invoice_number: collection.xpath('CommissionedOutstandingCollections//Item//ID')[i].content,
 				fixed_value: collection.xpath('CommissionedOutstandingCollections//GroupingCode')[i].content,
 				amount: collection.xpath('CommissionedOutstandingCollections//SubmittedAmount')[i].content,
@@ -66,13 +43,4 @@ module XmlParser
 			}
 		end
 	end
-
 end
-
-#a = Nori.new(:parser => :rexml).parse(File.read('./test-data.xml'))
-#binding.pry
-#XmlParser.parse('')
-# what about testing
-# is the XML api stable?
-
-
