6be85bb3a5383916b5bec06cf67774ad58d9c088
Erik <erikwjonsson@gmail.com>
2018-02-23 23:25:19 +0100

    Fixed failing request spec.

--- spec/requests/debtors_spec.rb
+++ spec/requests/debtors_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Debtors", type: :request do
   describe "GET /debtors" do
     it "works!" do
-      get debtors_path
+      get debtor_path_url
       expect(response).to have_http_status(200)
     end
   end
