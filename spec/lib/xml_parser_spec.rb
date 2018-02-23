bf053042787fde105fe17873a4e6c31120125b35
Erik <erikwjonsson@gmail.com>
2018-02-23 22:59:14 +0100

    Fixed failing xml_parser spec. - Fixed intendation for attributes hash.

--- spec/lib/xml_parser_spec.rb
+++ spec/lib/xml_parser_spec.rb
@@ -4,25 +4,25 @@ RSpec.describe XmlParser do
 
   let(:valid_attributes) {
     {
-			:system_id => "INK003    4628681287",
-			:customer_number => "4628681287",
-			:gender => "0001",
-			:first_name => "hans",
-			:last_name => "fakeson",
-			:iso_code_language => "AT",
-			:iso_code_communication_language => "de",
-			:iso_code_address_country => "AT",
-			:zip => "1111",
-			:city => "Wien",
-			:street => "test 123",
-			:house_number => "test 123",
-			:phone_number => "+4364412312312",
-			:mobile_phone_number => "+4364412312312",
-			:email_address => "hans.fakeson@fake.de",
-			:sap_invoice_number => "010000001282001",
-			:fixed_value => "01",
-			:amount => "269.88",
-			:date_of_export_to_debt_collection => "2017-12-19"
+      :system_id => "INK003    4628681287",
+      :customer_number => "4628681287",
+      :gender => "0001",
+      :first_name => "hans",
+      :last_name => "fakeson",
+      :iso_code_language => "AT",
+      :iso_code_communication_language => "de",
+      :iso_code_address_country => "AT",
+      :zip => "1111",
+      :city => "Wien",
+      :street => "test 123",
+      :house_number => "test 123",
+      :phone_number => "+4364412312312",
+      :mobile_phone_number => "+4364412312312",
+      :email_address => "hans.fakeson@fake.de",
+      :sap_invoice_number => "010000001282001",
+      :fixed_value => "01",
+      :amount => "269.88",
+      :date_of_export_to_debt_collection => "2017-12-19"
     }
   }
 
@@ -36,8 +36,8 @@ RSpec.describe XmlParser do
     end
 
     it "fails to save" do
-    	binding.pry
-			XmlParser.import 'test-data-faulty.xml'
+      expect { XmlParser.import 'test-data-faulty.xml' }.
+        to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Gender can't be blank, Gender is not included in the list")
       expect(Debtor.count).to eq 0
     end
   end
