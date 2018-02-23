50c4a4f12240f4bbcb8a1f302ecf19e54c747a3a
Erik <erikwjonsson@gmail.com>
2018-02-23 15:15:12 +0100

    - Removed comments about missing XML tags from xml_parser class. - Split street_name into street and house_number.

--- lib/xml_parser.rb
+++ lib/xml_parser.rb
@@ -17,24 +17,14 @@ module XmlParser
 				gender: collection.xpath("#{address}//PersonName//FormOfAddressCode")[i].content,
 				first_name: collection.xpath("#{address}//PersonName//GivenName")[i].content,
 				last_name: collection.xpath("#{address}//PersonName//FamilyName")[i].content,
-				#academic_title: collection.xpath("#{physical_address}//AcademicTitleCode")[i].content,
-				#academic_title_2: collection.xpath("#{physical_address}//AcademicTitleCode")[i].content,
-				# rename academic_title_2
-				# ACADEMIC TITES DOES NOT SEEM TO EXIST IN 
 				iso_code_language: collection.xpath("#{physical_address}//CountryName")[i].content,
-				# above: why did i not need to require this    CountryName languageCode="de"
 				iso_code_communication_language: collection.xpath("#{address}//Communication//CorrespondenceLanguageName")[i].attributes['languageCode'].value,
 				iso_code_address_country: collection.xpath("#{physical_address}//CountryCode")[i].content,
 				zip: collection.xpath("#{physical_address}//StreetPostalCode")[i].content,
 				city: collection.xpath("#{physical_address}//CityName")[i].content,
-				street: collection.xpath("#{physical_address}//StreetName")[i].content,
-				house_number: collection.xpath("#{physical_address}//StreetName")[i].content,
-				#second_address_line: collection.xpath("#{physical_address}//CareOfName")[i].content,
-				# CareOfName does not seem to exist in test-data.xml
+				street: collection.xpath("#{physical_address}//StreetName")[i].content.split(' ').first,
+				house_number: collection.xpath("#{physical_address}//StreetName")[i].content.split(' ').last,
 				phone_number: collection.xpath("#{address}//Telephone//SubscriberID")[i].content,
-				# review above
-				#mobile_phone_number: collection.xpath("#{address}//Communication//MobilePhone")[i].content,
-				# MobilePhone does not seem to exist.
 				email_address: collection.xpath("#{address}//Communication//Email//URI")[i].content,
 				sap_invoice_number: collection.xpath('CommissionedOutstandingCollections//Item//ID')[i].content,
 				fixed_value: collection.xpath('CommissionedOutstandingCollections//GroupingCode')[i].content,
