bacfacd10908aa0682e50ac60e5da472ddadee99
Erik <erikwjonsson@gmail.com>
2018-02-24 15:50:37 +0100

    Switched names of paths.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get '/debtors/:id', to: 'debtors#show', as: 'debtors_path'
-  get '/debtors', to: 'debtors#index', as: 'debtor_path'
+  get '/debtors/:id', to: 'debtors#show', as: 'debtor_path'
+  get '/debtors', to: 'debtors#index', as: 'debtors_path'
 end
--- spec/requests/debtors_spec.rb
+++ spec/requests/debtors_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Debtors", type: :request do
   describe "GET /debtors" do
     it "works!" do
-      get debtor_path_url
+      get debtors_path_url
       expect(response).to have_http_status(200)
     end
   end
